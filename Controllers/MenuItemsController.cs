11b2732502aefebf3569bc0c6fc25459e60fe6dc
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-02 18:55:32 +0200

    Added a customers controller

---
+++ Controllers/MenuItemsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantBookingMVC.Models;
+
+namespace RestaurantBookingMVC.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private string baseUri = "https://localhost:7037/"; // Vår bas url från vårt api
+        public MenuItemsController(HttpClient client)
+        {
+            _httpClient = client;
+        }
+        public async Task <IActionResult> Index()
+        {
+            ViewData["Title"] = "Menus | BG's"; // Sätter titeln i fliken
+
+            var response = await _httpClient.GetAsync($"{baseUri}api/MenuItems"); // Hämtar vår data från vårt API
+            var json = await response.Content.ReadAsStringAsync(); // Omvandlar datan till json sträng
+            var menuItemList = JsonConvert.DeserializeObject<List<MenuItem>>(json); // Omvandlar strängen till en list av menu item
+
+            return View(menuItemList);
+        }
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantBookingMVC.Models
+{
+    public class Table
+    {
+        public int TableID { get; set; }
+        public int TableNumber { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
