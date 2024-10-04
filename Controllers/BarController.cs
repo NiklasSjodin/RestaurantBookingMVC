51780e1ef99af6e330f95aaec6ca812c2c64bc9c
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-04 12:47:33 +0200

    Added  a bar controller for the bar section of the website

---
+++ Controllers/BarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantBookingMVC.Controllers
+{
+    public class BarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
