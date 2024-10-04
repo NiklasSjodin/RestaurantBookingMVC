7bebab3958f0664798b9509cb101074fee0e31ab
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-04 12:48:27 +0200

    Added a gift card controller for the gift card section on the website

---
+++ Controllers/GiftCardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantBookingMVC.Controllers
+{
+    public class GiftCardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
