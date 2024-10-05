84e3d50a6214410ff070d876e3c9b829c75b1005
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-05 23:57:00 +0200

    Added a reservation dto for viewing all reservations

---
+++ Models/DTOs/ReservationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantBookingMVC.Models.DTOs
+{
+    public class ReservationDTO
+    {
+        public int ReservationId { get; set; }
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public DateTime Time { get; set; }
+        public int TableNumber { get; set; }
+        public int NumberOfGuests { get; set; }
+    }
+}
