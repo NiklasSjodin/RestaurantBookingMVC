d48661faf5fb837550f8af7aef17c44cdc1a393e
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-05 23:56:14 +0200

    Added a admin controller with a view for all reservations

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantBookingMVC.Models;
+using RestaurantBookingMVC.Models.DTOs;
+
+namespace RestaurantBookingMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private string baseUri = "https://localhost:7037/"; // Vår bas url från vårt api
+        public AdminController(HttpClient client)
+        {
+            _httpClient = client;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Admin | BG's"; // Sätter titeln i fliken
+            var response = await _httpClient.GetAsync($"{baseUri}api/Reservations"); // Hämtar vår data från vårt API
+            var json = await response.Content.ReadAsStringAsync(); // Omvandlar datan till json sträng
+            var reservationsList = JsonConvert.DeserializeObject<List<ReservationDTO>>(json); // Omvandlar strängen till en list av menu item
+
+            return View(reservationsList);
+        }
+    }
+}
