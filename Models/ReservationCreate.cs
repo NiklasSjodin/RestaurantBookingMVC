44765b543acfde4c77a96cb72094b77a44034705
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-08 11:25:16 +0200

    Created a model that could be used in the create reservation

---
+++ Models/ReservationCreate.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace RestaurantBookingMVC.Models
+{
+    public class ReservationCreate
+    {
+        public int TableID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime Time { get; set; }
+        public int NumberOfGuests { get; set; }
+    }
+}
