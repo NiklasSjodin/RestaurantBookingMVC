b448393641b6732e8c52b101cb19b91cc60954e9
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-13 18:39:42 +0200

    Created an token handler that helps with the request and token, to not repeat code

---
+++ Handlers/JwtTokenHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Headers;
+
+namespace RestaurantBookingMVC.Handlers
+{
+    public class JwtTokenHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public JwtTokenHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _contextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = _contextAccessor.HttpContext.Request.Cookies["jwtToken"];
+            if(!string.IsNullOrEmpty(token) )
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
